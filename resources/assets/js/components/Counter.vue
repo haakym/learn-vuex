f911d9b60a030584b3bd1b13e0003323856c03fc
Dominic <haakym@gmail.com>
2017-09-27 15:20:08 +0100

    Learn Vuex With Me: Part 1

---
+++ resources/assets/js/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        {{ count }}
+        <div>
+            <button @click="increment">+1</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { mapState, mapMutations } from 'vuex';
+
+    export default {
+        computed: mapState(['count']),
+        methods: {
+            ...mapMutations(['increment'])
+        }
+    }
+</script>
---
+++ resources/assets/js/components/Other.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>The count from that other unrelated component is: {{ count }}</div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            count () {
+                return this.$store.state.count;
+            }
+        }
+    }
+</script>
